d678a9b29e71dd86d02f5c075d8000025af4bacd
Michal Vala <michal.vala@gmail.com>
2020-06-30 22:57:28 +0200

    some kotlin loopage

--- 04_Kotlin/src/Player.kt
+++ 04_Kotlin/src/Player.kt
@@ -1,12 +1,22 @@
 class Player(val name: String, private var lives: Int = 3) {
   var level = 1
   private var score = 0
+  private val pico = ArrayList<String>()
 
   override fun toString(): String {
     return """Player(name='$name', lives=$lives, level=$level, score=$score)"""
   }
 
 
+  fun add(bla: String) {
+    pico.add(bla)
+  }
+
+  fun print() {
+    for (eh in pico) {
+      println("ja $eh")
+    }
+  }
 }
 
 
@@ -17,4 +27,9 @@ fun main() {
   p1.level -= 1
   println(p1.level)
   println(p1.toString())
+
+  p1.add("eh")
+  p1.add("uh")
+
+  p1.print()\
 }
