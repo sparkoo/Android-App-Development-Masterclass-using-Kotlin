f85b101deba03317a0da67d16e29858cd8a2c4ea
Michal Vala <michal.vala@gmail.com>
2020-08-02 12:09:00 +0200

    08 - init rss reader top 10 apps downloader

---
+++ 08_RSSReader/app/src/main/java/cz/sparko/course/android/rssreader/MainActivity.kt
@@ -0,0 +1,11 @@
+package cz.sparko.course.android.rssreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
