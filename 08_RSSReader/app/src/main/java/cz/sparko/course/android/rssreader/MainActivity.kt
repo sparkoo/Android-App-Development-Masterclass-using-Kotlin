b155ed8de7e29bdd95d74d7fc0b91bb3c2530428
Michal Vala <michal.vala@gmail.com>
2020-08-02 12:22:07 +0200

    08 - AsyncTask class, basic log

--- 08_RSSReader/app/src/main/java/cz/sparko/course/android/rssreader/MainActivity.kt
+++ 08_RSSReader/app/src/main/java/cz/sparko/course/android/rssreader/MainActivity.kt
@@ -1,11 +1,32 @@
 package cz.sparko.course.android.rssreader
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+  @Suppress("PrivatePropertyName")
+  private val TAG = this::class.java.simpleName
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    Log.d(TAG, "onCreate: ")
+  }
+
+  private inner class DownloadData : AsyncTask<String, Void, String>() {
+    private val TAG = this::class.java.simpleName
+
+    override fun onPostExecute(result: String?) {
+      super.onPostExecute(result)
+    }
+
+    override fun doInBackground(vararg params: String?): String {
+      TODO("Not yet implemented")
+    }
+
   }
 }
