d819634644a077cc8ceded290cd94978fb165351
Michal Vala <michal.vala@gmail.com>
2020-09-29 06:11:23 +0200

    09 - playbackEventListener and playerStateChangeListener

--- 09_YoutubePlayer/app/src/main/java/cz/sparko/course/android/youtubeplayer/YoutubeActivity.kt
+++ 09_YoutubePlayer/app/src/main/java/cz/sparko/course/android/youtubeplayer/YoutubeActivity.kt
@@ -38,6 +38,9 @@ class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListen
     Log.d(TAG, "onInitializationSuccess: player is ${player?.javaClass}")
     Toast.makeText(this, "Initialized YoutubePlayer successfully", Toast.LENGTH_SHORT).show()
 
+    player?.setPlayerStateChangeListener(playerStateChangeListener)
+    player?.setPlaybackEventListener(playbackEventListener)
+
     if (!wasRestored) {
       player?.cueVideo(YOUTUBE_VIDEO_ID)
     }
@@ -57,6 +60,66 @@ class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListen
         "Error initializing the YoutubePlayer [$youTubeInitializationResult]",
         Toast.LENGTH_LONG
       ).show()
+    }
+  }
+
+  private val playbackEventListener = object: YouTubePlayer.PlaybackEventListener {
+    override fun onPlaying() {
+      Toast.makeText(this@YoutubeActivity, "PlaybackEventListener#onPlaying", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onPlaying: ")
+    }
+
+    override fun onStopped() {
+      Toast.makeText(this@YoutubeActivity, "PlaybackEventListener#onStopped", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onStopped: ")
+    }
+
+    override fun onPaused() {
+      Toast.makeText(this@YoutubeActivity, "PlaybackEventListener#onPaused", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onPaused: ")
+    }
+
+    override fun onBuffering(p0: Boolean) {
+      Toast.makeText(this@YoutubeActivity, "PlaybackEventListener#onBuffering($p0)", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onBuffering: $p0")
+    }
+
+    override fun onSeekTo(p0: Int) {
+      Toast.makeText(this@YoutubeActivity, "PlaybackEventListener#onSeekTo($p0)", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onSeekTo: $p0")
+    }
+
+  }
+
+  private val playerStateChangeListener = object: YouTubePlayer.PlayerStateChangeListener {
+    override fun onLoading() {
+      Toast.makeText(this@YoutubeActivity, "PlayerStateChangeListener#onLoading", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onLoading: ")
+    }
+
+    override fun onLoaded(p0: String?) {
+      Toast.makeText(this@YoutubeActivity, "PlayerStateChangeListener#onLoaded($p0)", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onLoaded: $p0")
+    }
+
+    override fun onAdStarted() {
+      Toast.makeText(this@YoutubeActivity, "PlayerStateChangeListener#onAdStarted", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onAdStarted: ")
+    }
+
+    override fun onVideoStarted() {
+      Toast.makeText(this@YoutubeActivity, "PlayerStateChangeListener#onVideoStarted", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onVideoStarted: ")
+    }
+
+    override fun onVideoEnded() {
+      Toast.makeText(this@YoutubeActivity, "PlayerStateChangeListener#onVideoEnded", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onVideoEnded: ")
+    }
+
+    override fun onError(p0: YouTubePlayer.ErrorReason?) {
+      Toast.makeText(this@YoutubeActivity, "PlayerStateChangeListener#onError($p0)", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "onError: $p0")
     }
   }
 }
