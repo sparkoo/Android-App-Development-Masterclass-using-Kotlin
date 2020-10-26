dba666faf239454f8539ea132a0e722d261d8631
Michal Vala <michal.vala@gmail.com>
2020-10-26 06:48:15 +0100

    10 - photo detail

--- 10_FlickrBrowser/app/src/main/java/cz/sparko/course/android/flickrbrowser/PhotoDetailsActivity.kt
+++ 10_FlickrBrowser/app/src/main/java/cz/sparko/course/android/flickrbrowser/PhotoDetailsActivity.kt
@@ -2,8 +2,11 @@ package cz.sparko.course.android.flickrbrowser
 
 import android.os.Bundle
 import android.util.Log
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
 private const val TAG = "PhotoDetailsActivity"
+
 class PhotoDetailsActivity : BaseActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,15 @@ class PhotoDetailsActivity : BaseActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_photo_details)
     activateToolbar(true)
+
+    (intent.getSerializableExtra(PHOTO_TRANSFER) as Photo?)?.let {
+      Log.d(TAG, "onCreate: transfered photo [$it]")
+      photo_title.text = it.title
+      photo_tags.text = it.tags
+      Picasso.get().load(it.image)
+        .error(R.drawable.image_placeholder)
+        .placeholder(R.drawable.image_placeholder)
+        .into(photo_image)
+    }
   }
 }
